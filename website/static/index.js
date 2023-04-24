6504ef286a8fcc81c00ce253dbc46c521c9863be
Yogesh Gathe <ygathe9@gmail.com>
2023-04-25 02:15:54 +0530

    Deleted flash message code from index.js file.

--- website/static/index.js
+++ website/static/index.js
@@ -1,13 +1,3 @@
-
-function showFlashMessage(element) {
-  var event = new CustomEvent('showFlashMessage');
-  element.dispatchEvent(event);
-};
-
-var flashMessages = document.getElementsByClassName('js-flash-message');
-//show first flash message avilable in your page
-showFlashMessage(flashMessages[0]);
-
 function deleteNote(noteId) {
     fetch("/delete-note", {
       method: "POST",
